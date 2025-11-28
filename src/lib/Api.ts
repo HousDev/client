8ad0ee869ea49ff167fba3caf25a77f767e08938
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2025-11-28 23:07:54 +0530

    edit https to http

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -40,7 +40,7 @@ const BASE =
   (typeof import.meta !== 'undefined' &&
     (import.meta as any).env &&
     (import.meta as any).env.VITE_API_URL) ||
-  'https://hously.in/api';
+  'http://hously.in/api';
   
 
 /* ----- token helpers ----- */
