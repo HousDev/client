1f8ecef8e3ebadda464a3b09ad793d24eae68a82
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2026-04-30 18:39:22 +0530

    fix the socket
    :qa

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,3 +1,12 @@
+// import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:4000", {
+//   transports: ["websocket"], // force websocket
+// });
+
+// export default socket;
+
+
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_SOCKET_API_URL, {
